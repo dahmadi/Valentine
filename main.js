8ad38f46f8ad9a4204c7d5c287e2f0fc5dce7200
Donya Ahmadi <donya.ahmadi832@gmail.com>
2026-02-06 18:11:29 -0600

    Update main.js

--- main.js
+++ main.js
@@ -11,72 +11,86 @@ const NO_PHRASES = [
   "Please don't do this to me, I'm sensitive",
 ];
 
+const firstImg =
+  "https://media.tenor.com/VIChDQ6ejRQAAAAj/jumping-bear-hearts-no-png.gif";
+const secondImg =
+  "https://media.tenor.com/f1xnRxTRxLAAAAAj/bears-with-kisses-bg.gif";
+
 function App() {
   const [noClicks, setNoClicks] = useState(0);
   const [isValentine, setIsValentine] = useState(false);
-
   const yesButtonSize = noClicks * 20 + 16;
 
-  const firstImg =
-    "https://media.tenor.com/VIChDQ6ejRQAAAAj/jumping-bear-hearts-no-png.gif";
-  const secondImg =
-    "https://media.tenor.com/f1xnRxTRxLAAAAAj/bears-with-kisses-bg.gif";
+  const containerStyle = {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    height: "100vh",
+    textAlign: "center",
+    fontFamily: "Arial, sans-serif",
+  };
 
-  return (
-    <div style={{
-      display: "flex", flexDirection: "column", alignItems: "center",
-      justifyContent: "center", height: "100vh", textAlign: "center"
-    }}>
-      {!isValentine ? (
-        <>
-          <img src={firstImg} alt="bear" />
-          <h1>Will you be my Valentine? 💘</h1>
-          <div>
-            <button
-              onClick={() => setIsValentine(true)}
-              style={{
-                fontSize: `${yesButtonSize}px`,
-                margin: "10px",
-                padding: "10px 20px",
-                backgroundColor: "green",
-                color: "white",
-                border: "none",
-                borderRadius: "5px",
-                cursor: "pointer",
-              }}
-            >
-              Yes
-            </button>
+  const yesStyle = {
+    fontSize: `${yesButtonSize}px`,
+    margin: "10px",
+    padding: "10px 20px",
+    backgroundColor: "green",
+    color: "white",
+    border: "none",
+    borderRadius: "5px",
+    cursor: "pointer",
+  };
 
-            <button
-              onClick={() => setNoClicks((p) => p + 1)}
-              style={{
-                fontSize: "16px",
-                margin: "10px",
-                padding: "10px 20px",
-                backgroundColor: "red",
-                color: "white",
-                border: "none",
-                borderRadius: "5px",
-                cursor: "pointer",
-              }}
-            >
-              {noClicks === 0
-                ? "No"
-                : NO_PHRASES[Math.min(noClicks - 1, NO_PHRASES.length - 1)]}
-            </button>
-          </div>
-        </>
-      ) : (
-        <>
-          <img src={secondImg} alt="kisses" />
-          <div style={{ fontSize: "48px", color: "pink", fontWeight: "bold" }}>
-            Yay!!! 💖🎉
-          </div>
-        </>
-      )}
-    </div>
+  const noStyle = {
+    fontSize: "16px",
+    margin: "10px",
+    padding: "10px 20px",
+    backgroundColor: "red",
+    color: "white",
+    border: "none",
+    borderRadius: "5px",
+    cursor: "pointer",
+  };
+
+  if (isValentine) {
+    return React.createElement(
+      "div",
+      { style: containerStyle },
+      React.createElement("img", { src: secondImg, alt: "kisses" }),
+      React.createElement(
+        "div",
+        { style: { fontSize: "48px", color: "pink", fontWeight: "bold" } },
+        "Yay!!! 💖🎉"
+      )
+    );
+  }
+
+  const noText =
+    noClicks === 0
+      ? "No"
+      : NO_PHRASES[Math.min(noClicks - 1, NO_PHRASES.length - 1)];
+
+  return React.createElement(
+    "div",
+    { style: containerStyle },
+    React.createElement("img", { src: firstImg, alt: "bear" }),
+    React.createElement("h1", null, "Will you be my Valentine? 💘"),
+    React.createElement(
+      "div",
+      null,
+      React.createElement(
+        "button",
+        { onClick: () => setIsValentine(true), style: yesStyle },
+        "Yes"
+      ),
+      React.createElement(
+        "button",
+        { onClick: () => setNoClicks((p) => p + 1), style: noStyle },
+        noText
+      )
+    )
   );
 }
 
-createRoot(document.getElementById("root")).render(<App />);
+createRoot(document.getElementById("root")).render(React.createElement(App));
